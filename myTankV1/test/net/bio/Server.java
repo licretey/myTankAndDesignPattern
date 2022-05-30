6d1be82a64bfb1b2069bff374f5ad6ae352e46ed
yangjp <2524228463@qq.com>
2022-05-30 23:03:25 +0800

    tcp三种模型基础，netty开发基础

--- myTankV1/test/net/bio/Server.java
+++ myTankV1/test/net/bio/Server.java
@@ -1,6 +1,7 @@
 package net.bio;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
@@ -11,12 +12,25 @@ public class Server {
     public static void main(String[] args) throws Exception{
         ServerSocket socket = new ServerSocket();
         socket.bind(new InetSocketAddress("127.0.0.1",9999));//绑定ip标识（对ip创建一个socket管理对象）
-        Socket accept = socket.accept();//阻塞监听（成功连接返回一个连接对象）
-        System.out.println("accepted a socked");
-        InputStreamReader isr = new InputStreamReader(accept.getInputStream());
-        BufferedReader br = new BufferedReader(isr);
-        String readLineStr = br.readLine();
-        System.out.println(readLineStr);
-        accept.close();
+        boolean started = true;
+
+        while (started){
+            Socket accept = socket.accept();//阻塞监听（成功连接返回一个连接对象）
+            System.out.println("accepted a socked");
+            new Thread(()->{
+                try {
+                    InputStreamReader isr = new InputStreamReader(accept.getInputStream());
+                    BufferedReader br = new BufferedReader(isr);
+                    // 阻塞读取
+                    // 一个客户不发送数据，后续会一直阻塞在这里（使用线程处理，但线程不可启太多）
+                    String readLineStr = br.readLine();
+                    System.out.println(readLineStr);
+
+                    accept.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
     }
 }
---
+++ myTankV1/test/net/netty/Client.java
@@ -0,0 +1,25 @@
+package net.netty;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) throws Exception{
+        Socket socket = new Socket("localhost",9999);//服务器地址
+        OutputStreamWriter osw = new OutputStreamWriter(socket.getOutputStream());
+        BufferedWriter bw = new BufferedWriter(osw);
+        bw.write("Hello I'm Netty Client!");
+        bw.newLine();
+        bw.flush();
+
+        InputStreamReader isr = new InputStreamReader(socket.getInputStream());
+        BufferedReader reader = new BufferedReader(isr);
+        String readLineStr =reader.readLine();
+        System.out.println(readLineStr);
+
+        bw.close();
+    }
+}
